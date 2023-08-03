22eabf1815a30363b279c121ff00d833a8cd4751
RAHMAHX1 <hafizur.rahman@abbvie.com>
2023-08-03 09:02:54 +0600

    Add Feature: Basic API calls with FastAPI

--- main.py
+++ main.py
@@ -4,4 +4,36 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message":"Welcome from FastAPI"}
+    return {"message":"Welcome from FastAPI. Let's start from here!"}
+
+@app.get("/about")
+async def root():
+    return {"message":"Hy! Thanks for trying.",
+            "team": "MrNothing",
+            "from": "Bangladesh"}
+
+@app.get("/about/developer")
+async def root():
+    return {"message":"This is Md Hafizur Rahman",
+            "Role": "Craftsman",
+            "from":"Bangladesh",
+            }
+
+@app.get("/features")
+async def root():
+    features = [
+        'Basic API Calls',
+        'Basic Routing',
+        'Advanced Routing',
+        'Validation',
+        'ORM',
+        'Database - Connections (SQLite, Postgres, MongoDB)',
+        'Authentication - Basic, OAuth',
+        'Authorization',
+        'Monitoring',
+        'Cashing',
+        'Deployment'
+    ]
+    return {"features":features}
+
+#Routing Sequence
